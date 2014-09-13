ced36f5db5b2b18a3507123acf6dc3cb9c024a64
Julian Hisdal Nymark <julianymark@gmail.com>
2014-09-14 00:50:00 +0200

    stuff

--- Player.py
+++ Player.py
@@ -21,16 +21,21 @@ def main():
     Rasterizer.setMousePosition(0,0)
     
     player_vel = player.getLinearVelocity(True)
-    print(player_vel)
 
     if keyboard.events[bge.events.COMMAKEY] == k:
-        if player_vel[1] > PLAYER_MAX_SPEED
-        player.applyForce([0,80,0], True)
+        if player_vel[1] < PLAYER_MAX_SPEED:
+            player.applyForce([0,80,0], True)
     if keyboard.events[bge.events.OKEY] == k:
-        player.applyForce([0,-80,0], True)
+        if player_vel[1] > -PLAYER_MAX_SPEED:
+            player.applyForce([0,-80,0], True)
     if keyboard.events[bge.events.AKEY] == k:
-        player.localLinearVelocity.x += -1
+        if player_vel[0] > -PLAYER_MAX_SPEED:
+            player.applyForce([-80,0,0], True)
     if keyboard.events[bge.events.EKEY] == k:
-        player.localLinearVelocity.x += 1
-
+        if player_vel[0] < PLAYER_MAX_SPEED:
+            player.applyForce([80,0,0], True)
+    if keyboard.events[bge.events.SPACEKEY] == k:
+        if player_vel[0] < PLAYER_MAX_SPEED:
+            player.applyForce([0,0,80], True)
+            
 main()
